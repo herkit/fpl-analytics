3423bac5109acac158dc08ebbf496c2eb2308040
Henrik Grotle <henrik@pswin.com>
2016-01-22 20:02:02 +0100

    added app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var auth = require("./lib/api/auth");
+var stats = require("./lib/api/playerstats");
+var Q = require("q");
+
+var retrieveManyPlayers = function(token) {
+  var promises = [];
+  for (var playerId = 1; playerId < 10; playerId++) {
+    promises.push(stats.getPlayerStats(token, playerId));
+  }
+  return Q.all(promises);
+}
+
+
+auth.authenticate(process.env.FPL_USERNAME, process.env.FPL_PASSWORD)
+.then(retrieveManyPlayers)
+.then(console.log)
+.then(null, console.error);
